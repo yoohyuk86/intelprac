f0756d926795757e457714dcb0c5b97bbf96596c
yoohyuk86 <fyuio2000@naver.com>
2024-12-13 16:51:46 +0900

    내 컴퓨터에서 이클립스로 파일을 업로드 하려고 함

---
+++ 컴퓨터에서 만든 폴더/Varbasic21.java
@@ -0,0 +1,21 @@
+
+public class Varbasic21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		int y = 200;
+		
+		System.out.println("x: " + x + "/ y: " + y );
+		
+		int temp = 0;
+		temp = x;
+		x = y;
+		y = temp;
+		
+
+		System.out.println("x: " + x + "/ y: " + y );
+
+	}
+	
+}
